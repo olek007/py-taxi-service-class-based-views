af83b99afe3db420c492290eee0730bb573b099e
Aleksander Rokita <kelor2@o2.pl>
2025-05-01 13:10:11 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def get_absolute_url(self) -> object:
+        return reverse("taxi:driver-detail", args=[str(self.id)])
+
+    class Meta:
+        ordering = ["username"]
 
 
 class Car(models.Model):
@@ -17,3 +27,9 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def get_absolute_url(self) -> object:
+        return reverse("taxi:car-detail", args=[str(self.id)])
+
+    class Meta:
+        ordering = ["model"]
--- taxi/views.py
+++ taxi/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
-
+    print(request)
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
@@ -13,3 +15,34 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all()
+    paginate_by = 5
+    context_object_name = "manufacturer_list"
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").all()
+    paginate_by = 5
+    context_object_name = "car_list"
+
+
+class CarDetailView(DetailView):
+    model = Car
+    context_object_name = "car"
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+    context_object_name = "driver_list"
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars").all()
+    context_object_name = "driver"
